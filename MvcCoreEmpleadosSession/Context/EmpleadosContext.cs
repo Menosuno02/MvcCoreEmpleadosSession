b55016f71d8ef5517ed270127146389fe8547b0c
Alejandro López Labajo <mainusone1@gmail.com>
2024-03-05 14:16:35 +0100

    Add project files.

---
+++ MvcCoreEmpleadosSession/Context/EmpleadosContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEmpleadosSession.Models;
+
+namespace MvcCoreEmpleadosSession.Context
+{
+    public class EmpleadosContext : DbContext
+    {
+        public EmpleadosContext(DbContextOptions<EmpleadosContext> options)
+            : base(options) { }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ MvcCoreEmpleadosSession/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEmpleadosSession.Context;
+using MvcCoreEmpleadosSession.Models;
+
+namespace MvcCoreEmpleadosSession.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            return await this.context.Empleados.ToListAsync();
+        }
+
+        public async Task<Empleado> FindEmpleadoAsync(int id)
+        {
+            return await this.context.Empleados
+                .FirstOrDefaultAsync(e => e.IdEmpleado == id);
+        }
+    }
+}
