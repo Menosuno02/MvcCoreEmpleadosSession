f2c0a8017eeb8562866df64a521964bb15a7a6b0
Alejandro López Labajo <mainusone1@gmail.com>
2024-03-06 10:21:02 +0100

    Commit

--- MvcCoreEmpleadosSession/Controllers/EmpleadosController.cs
+++ MvcCoreEmpleadosSession/Controllers/EmpleadosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosSession.Extensions;
 using MvcCoreEmpleadosSession.Models;
 using MvcCoreEmpleadosSession.Repositories;
 
@@ -13,8 +14,68 @@ namespace MvcCoreEmpleadosSession.Controllers
             this.repo = repo;
         }
 
-        public async Task<IActionResult> SessionSalarios
-            (int? salario)
+        public async Task<IActionResult> SessionEmpleadosOk(int? idempleado)
+        {
+            if (idempleado != null)
+            {
+                // Almacenamos lo
+                // mínimo, colección int
+                List<int> idsEmpleados;
+                if (HttpContext.Session.GetString("IDSEMPLEADOS") == null)
+                {
+                    idsEmpleados = new List<int>();
+                }
+                else
+                {
+                    idsEmpleados = HttpContext.Session.GetObject<List<int>>("IDSEMPLEADOS");
+                }
+                idsEmpleados.Add(idempleado.Value);
+                HttpContext.Session.SetObject("IDSEMPLEADOS", idsEmpleados);
+                ViewData["MENSAJE"] = "Empleados almacenados: " + idsEmpleados.Count();
+            }
+            List<Empleado> empleados = await this.repo.GetEmpleadosAsync();
+            return View();
+        }
+
+        public IActionResult EmpleadosAlmacenadosOk()
+        {
+            return View();
+        }
+
+
+
+
+
+        public async Task<IActionResult> SessionEmpleados(int? idEmpleado)
+        {
+            if (idEmpleado != null)
+            {
+                Empleado empleado =
+                    await this.repo.FindEmpleadoAsync(idEmpleado.Value);
+                List<Empleado> empleadosList;
+                if (HttpContext.Session.GetObject<List<Empleado>>("EMPLEADOS") != null)
+                {
+                    empleadosList =
+                        HttpContext.Session.GetObject<List<Empleado>>("EMPLEADOS");
+                }
+                else
+                {
+                    empleadosList = new List<Empleado>();
+                }
+                empleadosList.Add(empleado);
+                HttpContext.Session.SetObject("EMPLEADOS", empleadosList);
+                ViewData["MENSAJE"] = "Empleado " + empleado.Apellido + " almacenado";
+            }
+            List<Empleado> empleados = await this.repo.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public IActionResult EmpleadosAlmacenados()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SessionSalarios(int? salario)
         {
             if (salario != null)
             {
